8d0819ba2c3e3b44f11a0f518d3299f0140833d7
napharsh <harsh.goel@napworks.in>
2024-07-14 23:16:32 +0530

    Intro to express-generator

--- routes/index.js
+++ routes/index.js
@@ -3,12 +3,15 @@ var router = express.Router();
 const userModel = require("./users");
 const postsModel = require("./post");
 const passport = require('passport');
-const localStrategy = require("passport-local")
-passport.authenticate(new localStrategy(userModel.authenticate()));
+const localStrategy = require("passport-local");
+passport.use(new localStrategy(userModel.authenticate()));
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
+});
+router.get('/profile',isLoggedIn, function(req, res, next) {
+  res.send("Profile opened");
 });
 
 router.post("/register", function(req,res){
@@ -26,4 +29,22 @@ router.post("/register", function(req,res){
   })
 })
 
+router.post("/login", passport.authenticate("local", {
+  successRedirect: "profile",
+  failureRedirect: "/"
+}), function(req,res){
+
+})
+
+router.get("/logout", function(req,res){
+  req.logout(function(err){
+    if(err){ return next(err)}
+    res.redirect("/");
+  })
+})
+
+function isLoggedIn(req,res,next){
+  if(req.isAuthenticated()) return next();
+  res.redirect("/");
+}
 module.exports = router;
